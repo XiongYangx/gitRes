178ad0595d85d4df2596b76ed26bc7fcdad30fc7
XiongYangx <954496432@qq.com>
2018-04-12 21:09:46 +0800

    新建了一个项目

---
+++ gitStu/src/com/softeem/git/gitOne.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.softeem.git;
+
+/**
+ * @author XiongYan
+ *
+ * 2018Äê4ÔÂ12ÈÕ
+ */
+public class gitOne {
+	public String say(String name) {
+		return "hello "+name;
+	}
+
+}
